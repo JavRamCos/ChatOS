87602885e1786f3873e54c14a51cb87e2ef55128
JavierRamirezCospin <35494248+JavierRamirezCospin@users.noreply.github.com>
2022-04-26 00:26:54 -0600

    Updated Server.cpp
    - Added address format and Socket binding for SetInitParams function

--- Server.cpp
+++ Server.cpp
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 /* Clase Servidor */
 class Server {
     private:
         int port;
         int sockt;
+        struct sockaddr_in address;
     public:
         /* Constructor to define initial params */
         Server(int s_port) {
@@ -19,6 +21,16 @@ class Server {
             this->sockt = socket(AF_INET,SOCK_STREAM,0);
             if(this->sockt < 0) {
                 printf("> Error creating socket\n");
+                return -1;
+            }
+            /* Address format */
+            this->address.sin_family = AF_INET;
+            this->address.sin_port = htons(this->port);
+            this->address.sin_addr.s_addr = INADDR_ANY;
+            /* Socket binding */
+            int bind_flag = bind(this->sockt,(struct sockaddr*)&this->address,sizeof(this->address));
+            if(bind_flag < 0) {
+                printf("> Error binding socket\n");
                 return -1;
             }
             return 0;
