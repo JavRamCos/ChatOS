37b3b5a791498a9a698b004e7150530b03a90752
JavierRamirezCospin <35494248+JavierRamirezCospin@users.noreply.github.com>
2022-04-26 00:13:01 -0600

    updated Server.cpp
    - Added function to set Server initial parameters, in this case the socket creation

--- Server.cpp
+++ Server.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 /* Clase Servidor */
 class Server {
     private:
         int port;
+        int sockt;
     public:
         /* Constructor to define initial params */
         Server(int s_port) {
             this->port = s_port;
+        }
+
+        int SetInitParams() {
+            /* Creating socket connection */
+            this->sockt = socket(AF_INET,SOCK_STREAM,0);
+            if(this->sockt < 0) {
+                printf("> Error creating socket\n");
+                return -1;
+            }
+            return 0;
         }
 };
 
@@ -18,7 +31,7 @@ class Server {
 int main(int argc,char* argv[]) {
     /* Checking number of parameters */
     if(argc != 2) {
-        printf("Not enough arguments ...\n");
+        printf("> Not enough arguments ...\n");
         return EXIT_FAILURE;
     }
     /* Server object creation */
